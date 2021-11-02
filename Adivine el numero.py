6c88654923d9d55e2509d73c33d7be32706963ef
crltsnch <crltsnch@myuax.com>
2021-11-02 12:14:14 +0100

    main

--- Adivine el numero.py
+++ Adivine el numero.py
@@ -1,2 +1,9 @@
 import random
 numero = random.randint(0, 99)
+numero = int
+
+print(numero)
+print("piensa un numero entre" + str(0) + str(99))
+numero_usuario = input()
+
+while str(numero_usuario) > 99:
