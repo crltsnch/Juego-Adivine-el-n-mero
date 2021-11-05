fafbb92d4513900a3c395d8bfd9e24e26177ab81
crltsnch <crltsnch@myuax.com>
2021-11-05 11:43:55 +0100

    cambios 4

--- Adivine el numero.py
+++ Adivine el numero.py
@@ -1,9 +1,14 @@
 import random
 numero = random.randint(0, 99)
 numero = int ()
-intento = input("piensa un numero entre 0 y 99: ")
+intento = input ("piensa un numero entre 0 y 99: ")
 
-print(numero)
-print("piensa un numero entre 0 y 99: " + str(0) + str(99))
+print("numero" + str(0) + str(99))
 
-while str(intento) > 99:
+while int(intento) != numero:
+    if intento > numero:
+        print("Demasiado grande")
+    if intento < numero:
+        print("Demasiado pequeño")
+
+print("¡Ha ganado!")
